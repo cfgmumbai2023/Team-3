8a5a06045edd864f54daec4c01d122a8ac87d2c5
ihsnayvid <sri.divyanshi1920@gmail.com>
2023-07-08 23:40:33 +0530

    Login/Signup Merged

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import './styles/App.css';
+// import '../../App.css';
 import Header from './components/Header';
 import Login from './components/Login';
 import Home from './components/Home';
 import Register from './components/Register';
+import ForgetPasswordPage from "./components/ForgetPasswordPage.jsx";
 
 function App() {
   return (
@@ -13,10 +16,29 @@ function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/forget-password" element={<ForgetPasswordPage/>} />
       </Routes>
     </Router>
     </>
   );
+}
+
+const Footer = () => {
+  return (
+      <p className="text-center" style={ FooterStyle }>Designed & coded by Team: 3 <a href="#" target="_blank"></a></p>
+  )
+}
+
+const FooterStyle = {
+  background: "#222",
+  fontSize: ".8rem",
+  color: "#fff",
+  position: "absolute",
+  bottom: 0,
+  padding: "1rem",
+  margin: 0,
+  width: "100%",
+  opacity: ".5"
 }
 
 export default App;
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import '../styles/App.css';
 
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+
+import BackgroundImage from '../assets/bg.png';
+
+export default function LandingPage() {
+    return (
+        <header style={ HeaderStyle }>
+            <h1 className="main-title text-center">login / register page</h1>
+            <p className="main-para text-center">join us now and don't waste time</p>
+            <div className="buttons text-center">
+                <Link to="/login">
+                    <button className="primary-button">log in</button>
+                </Link>
+                <Link to="/register">
+                    <button className="primary-button" id="reg_btn"><span>register </span></button>
+                </Link>
+            </div>
+        </header>
+    )
 }
 
-export default Home;
+const HeaderStyle = {
+    width: "100%",
+    height: "100vh",
+    background: `url(${BackgroundImage})`,
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover"
+}
