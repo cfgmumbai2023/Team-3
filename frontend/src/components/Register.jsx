4a8f8cf9b794ff8591d47e936c561c93524d7ca5
Sumedha Panja <sumedhapanja.2001@gmail.com>
2023-07-09 02:36:19 +0530

    change in bg img

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -12,16 +12,32 @@ export default function SignUpPage() {
             <h5>Create your personal account</h5>
             <form action="/home">
                 <p>
-                    <label>Username</label><br/>
+                    <label>Role(Instructor/Coach)</label><br/>
                     <input type="text" name="first_name" required />
                 </p>
                 <p>
-                    <label>Email address</label><br/>
+                    <label>Name</label><br/>
                     <input type="email" name="email" required />
                 </p>
                 <p>
-                    <label>Password</label><br/>
+                    <label>Email</label><br/>
                     <input type="password" name="password" requiredc />
+                </p>
+                <p>
+                    <label>Password</label><br/>
+                    <input type="text" name="first_name" required />
+                </p>
+                <p>
+                    <label>License</label><br/>
+                    <input type="text" name="first_name" required />
+                </p>
+                <p>
+                    <label>FileUrl</label><br/>
+                    <input type="text" name="first_name" required />
+                </p>
+                <p>
+                    <label>Years of Experience</label><br/>
+                    <input type="text" name="first_name" required />
                 </p>
                 <p>
                     <input type="checkbox" name="checkbox" id="checkbox" required /> <span>I agree all statements in <a href="https://google.com" target="_blank" rel="noopener noreferrer">terms of service</a></span>.
