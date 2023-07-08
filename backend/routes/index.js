fbb5571effe140d87ff185e20a37fe652e8bbbd5
Kaveri0363 <92212208+Kaveri0363@users.noreply.github.com>
2023-07-08 23:18:53 +0530

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 const { curriculumRouter } = require("./curriculum");
-
+const {authRoutes}= require("./authRoutes")
 // Rest object
 const router = Router();
 
 router.use("/curriculum", curriculumRouter);
+router.use("/auth", authRoutes);
+
 
 module.exports = { router };
