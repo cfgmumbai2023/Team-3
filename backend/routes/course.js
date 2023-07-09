e56df9d773525c37904b7e7ff924a1c236666a4f
Kunal Joshi <mail.kunaljoshi@gmail.com>
2023-07-09 07:18:07 +0530

    feat: Added checks

--- backend/routes/course.js
+++ backend/routes/course.js
@@ -1,9 +1,17 @@
 const { Router } = require("express");
-const { addCourse, subscribeToCourse } = require("../controllers/course")
+const Curriculum = require('../models/Curriculum')
+const { addCourse, subscribeToCourse } = require("../controllers/course");
 
 const courseRouter = Router()
 
 courseRouter.post("/", addCourse)
 courseRouter.post("/:cId", subscribeToCourse)
+courseRouter.get("/:curId", async (req, res) => {
+    const { curId } = req.params
+    const curriculum = await Curriculum.findById(curId).populate('courses')
+    return res.status(201).send({
+        courses: curriculum.courses
+    })
+})
 
 module.exports = courseRouter
