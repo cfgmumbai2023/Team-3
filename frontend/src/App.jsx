f02632423ffb793eaa605ab4488b647d16136286
Animesh Nayak <nayakanimesh12@gmail.com>
2023-07-09 05:39:36 +0530

    testing commit for app.jsx file 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,65 @@ import Assessment from "./components/Assessment";
 import ChatBot from 'react-simple-chatbot';
 import { ThemeProvider } from 'styled-components';
 
+const steps = [
+  {
+      id: '0',
+      message: 'Hey Rookie!',
+
+      // This calls the next id
+      // i.e. id 1 in this case
+      trigger: '1',
+  }, {
+      id: '1',
+
+      // This message appears in
+      // the bot chat bubble
+      message: 'Please write your fieldname',
+      trigger: '2'
+  }, {
+      id: '2',
+
+      // Here we want the user
+      // to enter input
+      user: true,
+      trigger: '3',
+  }, {
+      id: '3',
+      message: " Hi {previousValue}, how can I help you?",
+      trigger: 4
+  }, {
+      id: '4',
+      options: [
+           
+          // When we need to show a number of
+          // options to choose we create alist
+          // like this
+          { value: 1, label: 'View Courses' },
+          { value: 2, label: 'Read Articles' },
+
+      ],
+      end: true
+  }
+];
+
+// Creating our own theme
+const theme = {
+  background: '#C9FF8F',
+  headerBgColor: '#197B22',
+  headerFontSize: '20px',
+  botBubbleColor: '#0F3789',
+  headerFontColor: 'white',
+  botFontColor: 'white',
+  userBubbleColor: '#FF5733',
+  userFontColor: 'white',
+};
+
+// Set some properties of the bot
+const config = {
+  botAvatar: "img.png",
+  floating: true,
+};
+
 function App() {
   return (
     <>
@@ -25,6 +84,18 @@ function App() {
         <Route path="/quiz" element={<Assessment/>} />
       </Routes>
     </Router>
+    <div className="App">
+            <ThemeProvider theme={theme}>
+                <ChatBot
+ 
+                    // This appears as the header
+                    // text for the chat bot
+                    headerTitle="Foot-Bot"
+                    steps={steps}
+                    {...config}
+                />
+            </ThemeProvider>
+        </div>
     </>
   );
 }
